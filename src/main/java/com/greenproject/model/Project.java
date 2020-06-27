3e7008903d0cc59e1fa131a21c4f8351684756eb
ranjit <ranjit.patel@billpay.de>
2020-06-27 09:12:19 +0200

    added lombok plugins

--- src/main/java/com/greenproject/model/Project.java
+++ src/main/java/com/greenproject/model/Project.java
@@ -1,10 +1,15 @@
 package com.greenproject.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 @Table(name = "project", schema = "greenfield")
+@Getter
+@Setter
 public class Project {
 
     @Id
@@ -25,68 +30,5 @@ public class Project {
     @Column(name = "isapproved")
     private Boolean isApproved;
 
-    public String getScale() {
-        return scale;
-    }
-
-    public void setScale(String scale) {
-        this.scale = scale;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Integer getCost() {
-        return cost;
-    }
-
-    public void setCost(Integer cost) {
-        this.cost = cost;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public Boolean getApproved() {
-        return isApproved;
-    }
-
-    public void setApproved(Boolean approved) {
-        isApproved = approved;
-    }
 
 }
